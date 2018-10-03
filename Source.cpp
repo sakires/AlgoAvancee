2e2578924bc164c8cb0351d53ffa6d5e35899c92
sakires <34483208+sakires@users.noreply.github.com>
2018-10-03 14:32:25 +0200

    Add files via upload

--- Source.cpp
+++ Source.cpp
@@ -21,44 +21,61 @@ void saisirControleEntierBorne(string message, int borneinf, int bornesup, int& 
 		cin >> nombre;
 	}
 }
-typedef struct {
+typedef struct maillon  {
 	int numero;
 	maillon *suivant;
-}maillon;
+};
 
-typedef struct {
+typedef struct FileAttente {
 	maillon *tete;
 	maillon *queue;
-}FileAttente;
+};
 
 
-void ajouterEnQueue(FileAttente li , int numero) {
+void ajouterEnQueue( FileAttente &li , int numero) {
 	maillon *sp;
 	sp = new maillon;
-	sp->numero = numero;
-	li.queue->suivant = sp;
-	li.queue = sp;
+	if (li.tete != NULL) {
+		sp->numero = numero;
+		li.queue->suivant = sp;
+		li.queue = sp;
+	}
+	else {
+		li.tete = new maillon;
+		li.queue = new maillon;
+		li.tete->numero = numero;
+		li.tete->suivant = li.queue;
+	}
+
 }
 
 int Consulter(FileAttente li) {
-	return li.tete->numero;
+	if (li.tete != NULL)
+		return li.tete->numero;
+	else
+		return 0;
 }
 
 int main() {
 	int exo;
-
+	FileAttente li ;
+	li.tete = NULL;
+	li.queue = NULL;
 	do {
-		saisirControleEntierBorne("\n1 recurcivite \n2 -puissance \n3 -PGCD \n4 quitter", 0, 4, exo);
+		saisirControleEntierBorne("\n1 ajouter \n2 -// \n3 -consulter \n4 // \n5 quitter", 0, 5, exo);
 		switch (exo)
 		{
 		case 1:
-			exercice1();
+			int element;
+			cout << " tapez un entier " << endl;
+			cin >> element;
+			ajouterEnQueue(li , element);
 			break;
 		case 2:
-			exercice2();
+			//exercice2();
 			break;
 		case 3:
-			//exercice3();
+			cout << Consulter(li);
 			break;
 
 		default:
