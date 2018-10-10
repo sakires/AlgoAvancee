7f5ba4327df85aafa504e0303cc32b0e33b07e6e
LeoLacoste <38657844+LeoLacoste@users.noreply.github.com>
2018-10-10 10:15:23 +0200

    Add files via upload

--- Source.cpp
+++ Source.cpp
@@ -21,120 +21,37 @@ void saisirControleEntierBorne(string message, int borneinf, int bornesup, int& 
 		cin >> nombre;
 	}
 }
-typedef struct maillon  {
-	int numero;
-	maillon *suivant;
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//	Structure de données représentant un maillon (avec chaine)
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+typedef struct Personne {
+	string name;
+	Personne *suivant;
 };
 
-
-typedef struct FileAttente {
-	maillon *tete;
-	maillon *queue;
-};
-
-
-
-
-
-void ajouterEnQueue( FileAttente &li , int numero) {
-	maillon *sp;
-	sp = new maillon;
-	if (li.tete != NULL) {
-		sp->numero = numero;
-		if (li.queue != NULL){
-			li.queue->suivant = sp;
-			li.queue = sp;
-		}
-		else {
-			li.tete->suivant = sp;
-			li.queue = sp;
-		}
-
-		sp->suivant = NULL;
-	}
-	else {
-		li.tete = new maillon;
-		li.tete->numero = numero;
-		li.tete->suivant = NULL;
-	}
-
-}
-void Sup_tete(FileAttente &li) {
-	if (li.tete != NULL) {
-		maillon *ap = li.tete->suivant;
-		delete li.tete;
-		li.tete = ap;
-	}
-}
-
-
-int Longueur(FileAttente li) {
-	int l;
-	maillon *sp = new maillon;
-	l = 0;
-	if (li.tete != NULL) {
-		l++;
-		sp = li.tete;
-		while (sp->suivant != NULL) {
-			sp = sp->suivant;
-			l = l + 1;
-		}
-	}
-	return l;
-}
-void Consulter(FileAttente li) {
-	if (li.tete != NULL)
-		cout <<"le premier element est "<< li.tete->numero<<endl;
-	else
-		cout << "il n y a pas d element dans la file" << endl;
-}
-
-int main() {
-	int exo;
-	FileAttente li ;
-	li.tete = NULL;
-	li.queue = NULL;
-	do {
-		saisirControleEntierBorne("\n1 ajouter \n2 -retirer \n3 -consulter \n4 longueur \n5 quitter", 0, 5, exo);
-		switch (exo)
-		{
-		case 1:
-			int element;
-			cout << " tapez un entier " << endl;
-			cin >> element;
-			ajouterEnQueue(li , element);
-			break;
-		case 2:
-			Sup_tete(li);
-			break;
-		case 3:
-			Consulter(li);
-			break;
-		case 4: cout <<"la longeur est de "<< Longueur(li) << endl;
-		default:
-			cout << "Quitter" << endl;
-			break;
-		}
-	} while (exo != 5);
-
-	system("pause");
-
-	return 0;
-}
-
-
-
-/////////////////////////////////////////// EXERCICE 2 ///////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//	Structure de données représentantla file d'attente
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 typedef struct FileAttentePerso {
 	Personne *tete;
 	Personne *queue;
 };
 
-typedef struct Personne {
-	string name;
-	Personne *suivant;
-};
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//	Procédure permettant d'ajouter un entier en queue de file c'est-à-dire à la fin de la file d'attente
+//	ENTREE : une variable numero de type entier qui est la valeur à insérer dans la file
+//	SORTIE : une variable li de type FileAttente qui représente la file d'attente
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 void ajouterEnQueuePerso(FileAttentePerso &li, string name) {
 	Personne *sp;
@@ -159,6 +76,15 @@ void ajouterEnQueuePerso(FileAttentePerso &li, string name) {
 	}
 
 }
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//	Procédure permettant de supprimer l'élement en tete de file c'est-à-dire en première position
+//	ENTREE : rien
+//	SORTIE : une variable li de type FileAttente
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 void Sup_tetePerso(FileAttentePerso &li) {
 	if (li.tete != NULL) {
 		Personne *ap = li.tete->suivant;
@@ -167,6 +93,13 @@ void Sup_tetePerso(FileAttentePerso &li) {
 	}
 }
 
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//	Procédure permettant de calculer la longueur de la liste
+//	ENTREE : une variable li de type FileAttente
+//	RETOUR :une varaible l de type entier qui correspond à la longueur de la liste
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 int LongueurPerso(FileAttentePerso li) {
 	int l;
@@ -182,9 +115,61 @@ int LongueurPerso(FileAttentePerso li) {
 	}
 	return l;
 }
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//	Procédure permettant de consulter le premier élément de la liste c'est -à-dire voir le contenu de li.tete
+//	ENTREE : une variable li de type FileAttente
+//	SORTIE : rien
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 void ConsulterPerso(FileAttentePerso li) {
 	if (li.tete != NULL)
 		cout << "le premier element est " << li.tete->name << endl;
 	else
 		cout << "il n y a pas d element dans la file" << endl;
 }
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//			PROGRAMME PRINCIPAL DE L'EXO 2
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+int main() {
+	int exo;
+	string element;
+	FileAttentePerso li;
+	li.tete = NULL;
+	li.queue = NULL;
+	do {
+		saisirControleEntierBorne("\n1 ajouter \n2 -retirer \n3 -consulter \n4 longueur \n5 quitter", 0, 5, exo);
+		switch (exo)
+		{
+		case 1:
+			
+			cout << " tapez une chaine " << endl;
+			cin >> element;
+			ajouterEnQueuePerso(li, element);
+			break;
+		case 2:
+			Sup_tetePerso(li);
+			break;
+		case 3:
+			ConsulterPerso(li);
+			break;
+		case 4: cout << "la longeur est de " << LongueurPerso(li) << endl;
+		default:
+			cout << "Quitter" << endl;
+			break;
+		}
+	} while (exo != 5);
+
+	system("pause");
+
+	return 0;
+}
+
+
+
